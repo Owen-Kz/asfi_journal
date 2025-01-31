9c6175838b39d006cb95b61dc83a999047c5ecda
Benson Michael Owen <lordkz19@gmail.com>
2025-01-31 09:19:43 +0100

    Manage issues page

--- manuscriptPortal/manage/index.php
+++ manuscriptPortal/manage/index.php
@@ -539,10 +539,10 @@
 <script src="../../front/public/js/vendor/wow.min.js?v=<?= time(); ?>"></script>
 <script src="../../js/queries/manageArticles.js" type="module"></script>
 
-<script src="https://cdn.quilljs.com/1.3.6/quill.js?"></script>
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 
 <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js'></script>
-<script type="module" src="../../js/forms/articleEdit.js?v=<?= time(); ?>"></script>
+<script type="module" src="../../js/forms/articleEdit.js"></script>
 
 <script type="module" src="../../js/forms/quill.js"></script>
     <script>
